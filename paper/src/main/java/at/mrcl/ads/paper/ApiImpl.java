cc73712b63cc0d535a1d38b5d0141795b448d269
mrcl <hey@mrcl.at>
2026-07-19 16:46:07 +0200

    Initialize project structure with API, Paper module, and build configurations.

---
+++ paper/src/main/java/at/mrcl/ads/paper/ApiImpl.java
@@ -0,0 +1,9 @@
+package at.mrcl.ads.paper;
+
+import at.mrcl.ads.api.API;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class ApiImpl implements API {
+    private final AdsPlugin plugin;
+}
