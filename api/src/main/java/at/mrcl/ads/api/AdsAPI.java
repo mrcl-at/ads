030f730c8094b3d0abcef93ec340709bb8ae5ef8
mrcl <hey@mrcl.at>
2026-07-20 14:53:59 +0200

    Annotate `setApi` method parameter with `@NonNull` to enforce null safety.

--- api/src/main/java/at/mrcl/ads/api/AdsAPI.java
+++ api/src/main/java/at/mrcl/ads/api/AdsAPI.java
@@ -1,13 +1,14 @@
 package at.mrcl.ads.api;
 
 import lombok.Getter;
+import org.jspecify.annotations.NonNull;
 
 @Getter
 public class AdsAPI {
 
     private static API api;
 
-    public static void setApi(API api) {
+    public static void setApi(@NonNull API api) {
         if (AdsAPI.api != null) throw new IllegalStateException("API already set!");
         AdsAPI.api = api;
     }
