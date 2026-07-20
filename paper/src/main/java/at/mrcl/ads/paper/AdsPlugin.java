e52ceb2f0cf0ef45ff0fae445f8768d615cc6e91
mrcl <hey@mrcl.at>
2026-07-20 14:18:22 +0200

    Integrate bStats for plugin metrics tracking and add necessary Maven configurations.

--- paper/src/main/java/at/mrcl/ads/paper/AdsPlugin.java
+++ paper/src/main/java/at/mrcl/ads/paper/AdsPlugin.java
@@ -1,14 +1,18 @@
 package at.mrcl.ads.paper;
 
 import at.mrcl.ads.api.AdsAPI;
+import org.bstats.bukkit.Metrics;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class AdsPlugin extends JavaPlugin {
+
+    private static final int BSTATS_ID = 32775;
 
     @Override
     public void onEnable() {
         try {
             AdsAPI.setApi(new ApiImpl(this));
+            new Metrics(this, BSTATS_ID);
         } catch (Exception exception) {
             getSLF4JLogger().error("Failed to enable plugin!", exception);
             getServer().getPluginManager().disablePlugin(this);
